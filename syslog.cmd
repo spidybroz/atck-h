39cce4673aaa47d3b1f2c3f705ff13bc3b10853d
spidybroz <spidybroz@outlook.com>
2026-02-27 15:13:59 +0300

    Update syslog.cmd to version 6.9+2

--- syslog.cmd
+++ syslog.cmd
@@ -1,10 +1,11 @@
 @echo off
-title SPYRUS 6.8+2
+title SPYRUS 6.9+2
 color 0a
 
-for /l %%i in (1,1,6) do (
-    start "SECURITY ALERT %%i" cmd /c "color 4 && title SPYRUS v6.8+2 && echo. && echo [!] SPYRUS Started && echo. && echo Breaching Device... && echo. && echo Accessing Camera... && echo Accessing Files... && echo Sending Payloads... && echo Accessing from C:\Program Files\Adobe... && echo. && echo System Breached..! && echo. && pause > nul"
+for /l %%i in (1,1,10) do (
+    start "SECURITY ALERT %%i" cmd /c "color 4 && title SPYRUS v6.9+2 && echo. && echo [!] SPYRUS Started && echo. && echo Breaching Device... && echo. && echo Accessing Camera... && echo Accessing Files... && echo Sending Payloads... && echo Accessing from C:\Program Files\Adobe... && echo. && echo System Breached..! && echo. && pause > nul"
 )
 
 timeout /t 2 > nul
+
 exit
