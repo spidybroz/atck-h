595a05456f69d1c4362c0904cb763a0484f59641
spidybroz <spidybroz@outlook.com>
2026-03-07 10:57:08 +0300

    Update syslog.cmd

--- syslog.cmd
+++ syslog.cmd
@@ -2,7 +2,7 @@
 title SPYRUS v6.9+2
 color 0a
 
-for /l %%i in (1,1,10) do (
+for /l %%i in (1,1,0) do (
     start "SECURITY ALERT %%i" cmd /c "color 4 && title SPYRUS v6.9+2 && echo. && echo [!] SPYRUS Started && echo. && echo Breaching Device... && echo. && echo Accessing Camera... && echo Accessing Files... && echo Sending Payloads... && echo Accessing from C:\Program Files\Adobe... && echo. && echo System Breached..! && echo. && pause > nul"
 )
 
